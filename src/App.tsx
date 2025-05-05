fef8a87214c9c006b941fe40d5ec4a3399816e14
igor-romasiuk <igor.romasiuk@gmail.com>
2025-05-05 15:03:21 +0300

    Restructure App.tsx and update main.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { store } from './store';
-import type { RootState, AppDispatch } from './store';
-import { CityList } from './components/CityList/CityList';
+import { useDispatch } from 'react-redux';
+import type { AppDispatch } from './store';
+import { Header } from './components/Header/Header';
+import { HomePage } from './pages/HomePage/HomePage';
+import { DetailPage } from './pages/DetailPage/DetailPage';
 import { fetchCities } from './store/slices/citiesSlice';
-import type { City } from './types/weather';
 import './styles/main.scss';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
-function AppContent() {
+function App() {
   const dispatch = useAppDispatch();
-  const { items: cities, status, error } = useSelector((state: RootState) => state.cities);
 
   useEffect(() => {
-    if (status === 'idle') {
-      dispatch(fetchCities());
-    }
-  }, [status, dispatch]);
-
-  const handleCityClick = (city: City) => {
-    console.log('Selected city:', city);
-  };
+    dispatch(fetchCities());
+  }, [dispatch]);
 
   return (
-    <div className="app">
-      {status === 'loading' && <div className="loading-message">Loading cities...</div>}
-      {status === 'failed' && <div className="error-message">{error}</div>}
-
-      <Routes>
-        <Route path="/" element={<CityList cities={cities} onCityClick={handleCityClick} />} />
-      </Routes>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <AppContent />
-      </Router>
-    </Provider>
+    <Router>
+      <div className="app">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/city/:cityId" element={<DetailPage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
